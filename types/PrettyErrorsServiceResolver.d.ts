91d3beccacf953e3e381efc9a63cd5dba3475d23
Donald Pakkies <donaldpakkies@gmail.com>
2021-10-09 11:23:06 +0200

    feat(build): compile imba and generate d.ts files

--- types/PrettyErrorsServiceResolver.d.ts
+++ types/PrettyErrorsServiceResolver.d.ts
@@ -1,3 +1,23 @@
 export default class PrettyErrorsServiceResolver extends ServiceResolver {
+    /**
+    @param {Error} response
+    @param {FormRequest} request
+    @param {FastifyReply} reply
+    */
+    errorHandler(response: Error, request: FormRequest, reply: FastifyReply): boolean | Promise<boolean>;
+    /**
+    @param {Error} response
+    @param {FormRequest} request
+    @param {FastifyReply} reply
+    */
+    handleProductionErrors(response: Error, request: FormRequest, reply: FastifyReply): boolean;
+    /**
+    @param {Error} response
+    @param {FormRequest} request
+    @param {FastifyReply} reply
+    */
+    handleDevelopmentErrors(response: Error, request: FormRequest, reply: FastifyReply): Promise<boolean>;
 }
 import { ServiceResolver } from "@formidablejs/framework";
+import { FormRequest } from "@formidablejs/framework";
+import { FastifyReply } from "@formidablejs/framework";
