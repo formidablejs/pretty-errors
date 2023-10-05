ad10dc8dc3eeb623c59453787f84e6b23827f257
Donald Pakkies <donaldpakkies@gmail.com>
2023-10-05 08:58:58 +0200

    chore: update types

--- types/HtmlResponse.d.ts
+++ types/HtmlResponse.d.ts
@@ -3,7 +3,7 @@ export class HtmlResponse extends Response {
     /**
     @param {FastifyReply} reply
     */
-    toResponse(reply: FastifyReply): FastifyReply<import("http").Server, import("http").IncomingMessage, import("http").ServerResponse, import("fastify/types/route").RouteGenericInterface, any>;
+    toResponse(reply: FastifyReply): FastifyReply<import("http").Server, import("http").IncomingMessage, import("http").ServerResponse, import("fastify").RouteGenericInterface, any, import("fastify").FastifySchema, import("fastify").FastifyTypeProviderDefault, unknown>;
 }
 import { Response } from "@formidablejs/framework";
 import { FastifyReply } from "@formidablejs/framework";
